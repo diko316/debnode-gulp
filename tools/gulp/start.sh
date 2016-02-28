22106cdbc844632efa004f7f0447010350a86c7f
Diko the tech slave <diko316@gmail.com>
2016-02-28 21:28:32 +0800

    fixed package.json and gulp/start.sh to post install node modules and bower tools

--- tools/gulp/start.sh
+++ tools/gulp/start.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
 CURRENT_CWD=$(pwd)
+
 ##########################################
-# validate gulp file
+# validate gulp directory
 ##########################################
 if [ ! -d "${GULP_SOURCE}" ]; then
     echo "${GULP_SOURCE} is not a directory" >&2
     exit 1
 fi
 
+##########################################
+# validate package.json
+##########################################
+PACKAGE_FILE="${GULP_SOURCE}/package.json"
+if [ ! -r "${PACKAGE_FILE}" ] || [ ! -f "${PACKAGE_FILE}" ]; then
+    echo "${PACKAGE_FILE} is not a file or not accessible" >&2
+    exit 1
+fi
+
+##########################################
+# validate gulp file
+##########################################
 GULP_FILE=${GULP_SOURCE}/gulpfile.js
 if [ ! -r "${GULP_FILE}" ] || [ ! -f "${GULP_FILE}" ]; then
     echo "${GULP_FILE} is not a file or not accessible" >&2
     exit 1
 fi
 
+
 cd "${GULP_SOURCE}"
-gulp run
+npm run start
 cd "${CURRENT_CWD}"
